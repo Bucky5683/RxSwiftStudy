520edc51b584a4e9e27d813b1ad2cf226d48c5bf
Bucky <bucky5683@gmail.com>
2024-03-11 10:24:35 +0900

    Observable 공부
    - next, error, complete가 무엇인지
    - subscibe를 어떻게 사용하는지
    - 실제 코드로 어떻게 구현하는지

--- RxSwiftStudy/ViewController.swift
+++ RxSwiftStudy/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
+    @IBOutlet weak var logLabel: UILabel!
+    var disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.ArrayCheck()
     }
-
-
+    
+    func ArrayCheck() {
+        self.checkArrayObservable(items: [4, 3, 1, 5, 2])
+            .subscribe { event in
+                switch event {
+                case .next(let value):
+                    print(value)
+                    self.logLabel.text = "\(value)"
+                case .error(let error):
+                    print(error)
+                    self.logLabel.text = "\(error)"
+                case .completed:
+                    print("completed")
+                    self.logLabel.text = "completed"
+                }
+            }
+            .disposed(by: self.disposeBag)
+    }
+    
+    func checkArrayObservable(items: [Int]) -> Observable<Int> {
+        //배열의 엘리멘트 중에 0이 나오면 에러가 나는 Observable
+        //rx에서 지원하는 operater도 있지만, 여기선 없이 생성
+        return Observable<Int>.create { observer -> Disposable in
+            for item in items {
+                if item == 0 {
+                    observer.onError(NSError(domain: "ERROR: value is zero.", code: 0, userInfo: nil))
+                    break
+                }
+                observer.onNext(item)
+                sleep(1)
+            }
+            observer.onCompleted()
+            return Disposables.create()
+        }
+    }
 }
 
