532633f485f103250fd4deb53fe9d3f5054a1025
Bucky <bucky5683@gmail.com>
2024-03-11 10:43:59 +0900

    Subscribe, Disposable

--- RxSwiftStudy/ViewController.swift
+++ RxSwiftStudy/ViewController.swift
@@ -10,48 +10,36 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
-    @IBOutlet weak var logLabel: UILabel!
-    var disposeBag = DisposeBag()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        self.ArrayCheck()
-    }
+  var disposeBag = DisposeBag()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    func ArrayCheck() {
-        self.checkArrayObservable(items: [4, 3, 1, 5, 2])
-            .subscribe { event in
-                switch event {
-                case .next(let value):
-                    print(value)
-                    self.logLabel.text = "\(value)"
-                case .error(let error):
-                    print(error)
-                    self.logLabel.text = "\(error)"
-                case .completed:
-                    print("completed")
-                    self.logLabel.text = "completed"
-                }
-            }
-            .disposed(by: self.disposeBag)
-    }
+    test()
+  }
+  
+  deinit {
+    print("deinit ViewController")
+  }
+
+  func test() {
+    Observable<Int>.interval(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance)
+      .take(10)
+      .subscribe(onNext: { value in
+        print(value)
+      }, onError: { error in
+        print(error)
+      }, onCompleted: {
+        print("onCompleted")
+      }, onDisposed: {
+        print("“onDisposed”")
+      })
+      .disposed(by: disposeBag)
     
-    func checkArrayObservable(items: [Int]) -> Observable<Int> {
-        //배열의 엘리멘트 중에 0이 나오면 에러가 나는 Observable
-        //rx에서 지원하는 operater도 있지만, 여기선 없이 생성
-        return Observable<Int>.create { observer -> Disposable in
-            for item in items {
-                if item == 0 {
-                    observer.onError(NSError(domain: "ERROR: value is zero.", code: 0, userInfo: nil))
-                    break
-                }
-                observer.onNext(item)
-                sleep(1)
-            }
-            observer.onCompleted()
-            return Disposables.create()
-        }
+    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+      UIApplication.shared.keyWindow?.rootViewController = nil
     }
+  }
 }
 
